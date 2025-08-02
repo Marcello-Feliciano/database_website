659db9f0a6b92030f0376baa59a15f5709c16711
Marcello-Feliciano <165084564+Marcello-Feliciano@users.noreply.github.com>
2025-08-02 13:07:03 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,10 @@
-const firebaseConfig = {
+// firebaseConfig.js
+export const firebaseConfig = {
   apiKey: "AIzaSyBpFQYyFyMQWUHjNuIwMyb8UrT7l92ASao",
   authDomain: "gmbcreviews.firebaseapp.com",
   projectId: "gmbcreviews",
-  storageBucket: "gmbcreviews.firebasestorage.app",
+  storageBucket: "gmbcreviews.appspot.com",
   messagingSenderId: "156411101412",
   appId: "1:156411101412:web:dfea6aa2c4b3518a042cf1",
   measurementId: "G-DH9W04ZEWL"
 };
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
