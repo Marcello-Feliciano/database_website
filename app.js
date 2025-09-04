e6482edf0e0427848f17fb8dcc8220ba625c1f47
Marcello-Feliciano <165084564+Marcello-Feliciano@users.noreply.github.com>
2025-09-04 11:37:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBpFQYyFyMQWUHjNuIwMyb8UrT7l92ASao",
   authDomain: "gmbcreviews.firebaseapp.com",
   projectId: "gmbcreviews",
-  storageBucket: "gmbcreviews.firebasestorage.app",
+  storageBucket: "gmbcreviews.appspot.com",
   messagingSenderId: "156411101412",
   appId: "1:156411101412:web:dfea6aa2c4b3518a042cf1",
   measurementId: "G-DH9W04ZEWL",
@@ -245,3 +245,4 @@ onAuthStateChanged(auth, (user) => {
     appScreen.style.display = "none";
   }
 });
+
